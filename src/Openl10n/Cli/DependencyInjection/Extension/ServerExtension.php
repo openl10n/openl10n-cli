d05dcb87f0548bf0ffc2ad229bec9184b7ccdd8f
Julien Deniau <julien.deniau@gmail.com>
2014-06-25 15:52:56 +0200

    Fix PSR-2 compliance in file

--- src/Openl10n/Cli/DependencyInjection/Extension/ServerExtension.php
+++ src/Openl10n/Cli/DependencyInjection/Extension/ServerExtension.php
@@ -34,21 +34,23 @@ class ServerExtension implements ExtensionInterface
 		$node
             ->beforeNormalization()
             ->ifString()
-                ->then(function($v) {
-                    $home = getenv('HOME');
-                    $filepath = $home.'/.openl10n/server.conf';
-                    $data = array();
-                    if (file_exists($filepath)) {
-                        $data = parse_ini_file($filepath, true);
-                    }
-                    if (isset($data[$v])) {
-                        return $data[$v];
-                    }
+                ->then(
+                    function ($v) {
+                        $home = getenv('HOME');
+                        $filepath = $home.'/.openl10n/server.conf';
+                        $data = array();
+                        if (file_exists($filepath)) {
+                            $data = parse_ini_file($filepath, true);
+                        }
+                        if (isset($data[$v])) {
+                            return $data[$v];
+                        }
 
-                    return array(
-                        'hostname' => $v
-                    );
-                })
+                        return array(
+                            'hostname' => $v
+                        );
+                    }
+                )
             ->end()
             ->children()
                 ->scalarNode('hostname')
@@ -68,7 +70,11 @@ class ServerExtension implements ExtensionInterface
                     // interpreted as boolean, then cast automatically.
                     ->beforeNormalization()
                     ->ifString()
-                        ->then(function($v) { return (boolean) $v; })
+                        ->then(
+                            function ($v) {
+                                return (boolean) $v;
+                            }
+                        )
                     ->end()
                     ->defaultFalse()
                 ->end()
