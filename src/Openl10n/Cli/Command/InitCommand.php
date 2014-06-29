c143a0ac3e0c6a34318f8c2807b2dc37406d2f23
Benoît Durand <bdurand-github@ssji.net>
2014-06-29 18:28:18 +0200

    Add init command to create basic config file

--- src/Openl10n/Cli/Command/InitCommand.php
+++ src/Openl10n/Cli/Command/InitCommand.php
@@ -4,7 +4,9 @@ namespace Openl10n\Cli\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class InitCommand extends Command
 {
@@ -15,6 +17,15 @@ class InitCommand extends Command
     {
         $this
             ->setName('init')
+            ->setDefinition(array(
+                new InputOption('project', null, InputOption::VALUE_REQUIRED, 'Slug of the project'),
+                new InputOption('hostname', null, InputOption::VALUE_REQUIRED, 'Set server hostname'),
+                new InputOption('port', null, InputOption::VALUE_REQUIRED, 'Specific port for the server', 80),
+                new InputOption('ssl', null, InputOption::VALUE_NONE, 'Use SSL'),
+                new InputOption('username', null, InputOption::VALUE_REQUIRED, 'Set server user'),
+                new InputOption('password', null, InputOption::VALUE_REQUIRED, 'Set server password'),
+                new InputOption('files', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Pattern of translation ressources, e.g. "locales/<locale>.yml"')
+            ))
         ;
     }
 
@@ -23,5 +34,88 @@ class InitCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $dialog = $this->getHelperSet()->get('dialog');
+        $options = $input->getOptions();
+
+        if (null == $options['project']) {
+            $project = basename(realpath('.'));
+            $options['project'] = $dialog->ask($output, "<info>Project's slug</info> [<comment>$project</comment>]: ", $project);
+        }
+        if (null == $options['hostname']) {
+            $options['hostname'] = $dialog->ask($output, '<info>Hostname</info> [<comment>openl10n.dev</comment>]: ', 'openl10n.dev');
+        }
+        if (false == $options['ssl']) {
+            $options['ssl'] = $dialog->askConfirmation($output, '<info>Enable ssl</info> [<comment>no</comment>]? ', false);
+        }
+        if (80 == $options['port']) {
+            $port = $options['ssl'] ? 443 : 80;
+            $options['port'] = $dialog->askAndValidate(
+                $output,
+                "<info>Port</info> [<comment>$port</comment>]: ",
+                function ($answer) {
+                    if (!is_int($answer) && !ctype_digit($answer)) {
+                        throw new \RuntimeException('The port must be an integer.');
+                    }
+                    return $answer;
+                },
+                false,
+                $port
+            );
+        }
+        if (null == $options['username']) {
+            $user = get_current_user();
+            $options['username'] = $dialog->ask($output, "<info>Username</info> [<comment>$user</comment>]: ", $user);
+        }
+        if (null == $options['password']) {
+            $options['password'] = $dialog->askHiddenResponseAndValidate(
+                $output,
+                '<info>Password</info> []: ',
+                function ($answer) {
+                    if ('' == trim($answer)) {
+                        throw new \RuntimeException('The password can not be empty.');
+                    }
+                    return $answer;
+                },
+                false,
+                false
+            );
+        }
+
+        if (null == $options['files']) {
+            $output->writeln('');
+            while (null !== $file = $dialog->ask($output, '<info>Pattern file</info> []: ')) {
+                if (false !== $file) {
+                    $options['files'][] = $file;
+                }
+            }
+        }
+
+        $config = array(
+            'server' => array(
+               'hostname' => $options['hostname'],
+               'port' => (int) $options['port'],
+               'use_ssl' => (bool) $options['ssl'],
+               'username' => $options['username'],
+               'password' => $options['password'],
+            ),
+            'project' => $options['project'],
+        );
+        if (null !== $options['files']) {
+            foreach ($options['files'] as $file) {
+                $config['files'][] = ['pattern' => $file];
+            }
+        }
+        if ((80 == $options['port'] && false == $options['ssl'])
+            || (443 == $options['port'] && true == $options['ssl'])
+        ) {
+            unset($config['server']['port']);
+        }
+        if (false == $options['ssl']) {
+            unset($config['server']['use_ssl']);
+        }
+
+        file_put_contents('./openl10n.yml', Yaml::dump($config, 3));
+
+        // TODO Ask to create project by api
     }
 }
