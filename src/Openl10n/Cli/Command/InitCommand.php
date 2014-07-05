b44c522d210fbeee540268c5ce730f090d25f0fd
Matthieu Moquet <matthieu@moquet.net>
2014-07-05 10:44:14 +0200

    Use strict comparisons

--- src/Openl10n/Cli/Command/InitCommand.php
+++ src/Openl10n/Cli/Command/InitCommand.php
@@ -2,8 +2,8 @@
 
 namespace Openl10n\Cli\Command;
 
-use Openl10n\Sdk\Model\Project;
 use GuzzleHttp\Exception\ClientException;
+use Openl10n\Sdk\Model\Project;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -22,7 +22,7 @@ class InitCommand extends AbstractCommand
             ->setDefinition(array(
                 new InputOption('project', null, InputOption::VALUE_REQUIRED, 'Slug of the project'),
                 new InputOption('hostname', null, InputOption::VALUE_REQUIRED, 'Set server hostname'),
-                new InputOption('port', null, InputOption::VALUE_REQUIRED, 'Specific port for the server', 80),
+                new InputOption('port', null, InputOption::VALUE_REQUIRED, 'Specific port for the server'),
                 new InputOption('ssl', null, InputOption::VALUE_NONE, 'Use SSL'),
                 new InputOption('username', null, InputOption::VALUE_REQUIRED, 'Set server user'),
                 new InputOption('password', null, InputOption::VALUE_REQUIRED, 'Set server password'),
@@ -39,17 +39,17 @@ class InitCommand extends AbstractCommand
         $dialog = $this->getHelperSet()->get('dialog');
         $options = $input->getOptions();
 
-        if (null == $options['project']) {
+        if (null === $options['project']) {
             $project = basename(realpath('.'));
             $options['project'] = $dialog->ask($output, "<info>Project's slug</info> [<comment>$project</comment>]: ", $project);
         }
-        if (null == $options['hostname']) {
+        if (null === $options['hostname']) {
             $options['hostname'] = $dialog->ask($output, '<info>Hostname</info> [<comment>openl10n.dev</comment>]: ', 'openl10n.dev');
         }
-        if (false == $options['ssl']) {
+        if (false === $options['ssl']) {
             $options['ssl'] = $dialog->askConfirmation($output, '<info>Enable ssl</info> [<comment>no</comment>]? ', false);
         }
-        if (80 == $options['port']) {
+        if (null === $options['port']) {
             $port = $options['ssl'] ? 443 : 80;
             $options['port'] = $dialog->askAndValidate(
                 $output,
@@ -58,24 +58,29 @@ class InitCommand extends AbstractCommand
                     if (!is_int($answer) && !ctype_digit($answer)) {
                         throw new \RuntimeException('The port must be an integer.');
                     }
-                    return $answer;
+
+                    return (int) $answer;
                 },
                 false,
                 $port
             );
+        } else {
+            $options['port'] = (int) $options['port'];
         }
-        if (null == $options['username']) {
+
+        if (null === $options['username']) {
             $user = get_current_user();
             $options['username'] = $dialog->ask($output, "<info>Username</info> [<comment>$user</comment>]: ", $user);
         }
-        if (null == $options['password']) {
+        if (null === $options['password']) {
             $options['password'] = $dialog->askHiddenResponseAndValidate(
                 $output,
                 '<info>Password</info> []: ',
                 function ($answer) {
-                    if ('' == trim($answer)) {
+                    if ('' === trim($answer)) {
                         throw new \RuntimeException('The password can not be empty.');
                     }
+
                     return $answer;
                 },
                 false,
@@ -83,7 +88,7 @@ class InitCommand extends AbstractCommand
             );
         }
 
-        if (null == $options['files']) {
+        if (empty($options['files'])) {
             $output->writeln('');
             while (null !== $file = $dialog->ask($output, '<info>Pattern file</info> []: ')) {
                 if (false !== $file) {
@@ -102,17 +107,18 @@ class InitCommand extends AbstractCommand
             ),
             'project' => $options['project'],
         );
+
         if (null !== $options['files']) {
             foreach ($options['files'] as $file) {
                 $config['files'][] = ['pattern' => $file];
             }
         }
-        if ((80 == $options['port'] && false == $options['ssl'])
-            || (443 == $options['port'] && true == $options['ssl'])
+        if ((80 === $options['port'] && !$options['ssl']) ||
+            (443 === $options['port'] && $options['ssl'])
         ) {
             unset($config['server']['port']);
         }
-        if (false == $options['ssl']) {
+        if (!$options['ssl']) {
             unset($config['server']['use_ssl']);
         }
 
@@ -121,14 +127,17 @@ class InitCommand extends AbstractCommand
         if (!$dialog->askConfirmation($output, '<info>Do you confirm generation</info> [<comment>yes</comment>]? ')) {
             return 1;
         }
+
         file_put_contents('./openl10n.yml', $content);
 
         $projectApi = $this->get('openl10n.api')->getEntryPoint('project');
+
         try {
             $project = $projectApi->get($config['project']);
-            return ;
+
+            return;
         } catch (ClientException $e) {
-            if ('404' != $e->getResponse()->getStatusCode()) {
+            if ('404' !== $e->getResponse()->getStatusCode()) {
                 throw $e;
             }
         }
@@ -148,6 +157,7 @@ class InitCommand extends AbstractCommand
                 $projectApi->create($project);
             } catch (\Exception $e) {
                 $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+
                 return 1;
             }
         }
