977722bf4f07e4632e36ee3b0f53755a94793469
Matthieu Moquet <matthieu@moquet.net>
2014-07-26 17:08:31 +0200

    Read config file if exists in init command

--- src/Openl10n/Cli/Command/InitCommand.php
+++ src/Openl10n/Cli/Command/InitCommand.php
@@ -4,7 +4,6 @@ namespace Openl10n\Cli\Command;
 
 use GuzzleHttp\Exception\ClientException;
 use Openl10n\Sdk\Model\Project;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,7 +30,7 @@ class InitCommand extends AbstractCommand
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function interact(InputInterface $input, OutputInterface $output)
     {
         $server = [
             'scheme' => null,
@@ -51,17 +50,30 @@ class InitCommand extends AbstractCommand
         $configurationLoader = $this->get('configuration.loader');
         $dialog = $this->getHelperSet()->get('dialog');
 
+        // Init configuration by manually read configuration file (if already exists)
+        $configPathname = $configurationLoader->getConfigurationFilepath();
+        if (is_file($configPathname)) {
+            $this->configuration = Yaml::parse($configPathname);
+
+            // If no URL specified then don't overwrite configuration
+            if (null === $url) {
+                return;
+            }
+        } else {
+            $this->configuration['server'] = [];
+            $this->configuration['files'] = [];
+        }
+
         // Server
-        $this->configuration['server'] = [];
         if (null !== $hostname = $server['host']) {
             $this->configuration['server']['hostname'] = $hostname;
-        } else {
+        } elseif (!isset($this->configuration['server']['hostname'])) {
             $this->configuration['server']['hostname'] = $dialog->ask($output, '<info>Hostname</info> [<comment>openl10n.dev</comment>]: ', 'openl10n.dev');
         }
 
         if (null !== $scheme = $server['scheme']) {
             $this->configuration['server']['use_ssl'] = 'https' === $scheme;
-        } else {
+        } elseif (!isset($this->configuration['server']['use_ssl'])) {
             $this->configuration['server']['use_ssl'] = $dialog->askConfirmation($output, '<info>Enable ssl</info> [<comment>no</comment>]? ', false);
         }
 
@@ -75,14 +87,14 @@ class InitCommand extends AbstractCommand
 
         if (null !== $username = $server['user']) {
             $this->configuration['server']['username'] = $username;
-        } else {
+        } elseif (!isset($this->configuration['server']['username'])) {
             $currentUser = get_current_user();
             $this->configuration['server']['username'] = $dialog->ask($output, "<info>Username</info> [<comment>$currentUser</comment>]: ", $currentUser);
         }
 
         if (null !== $password = $server['pass']) {
             $this->configuration['server']['password'] = $password;
-        } else {
+        } elseif (!isset($this->configuration['server']['password'])) {
             $currentUser = get_current_user();
             $this->configuration['server']['password'] = $dialog->askHiddenResponseAndValidate(
                 $output,
@@ -102,13 +114,12 @@ class InitCommand extends AbstractCommand
         // Project
         if (null !== $projectSlug = $input->getArgument('project')) {
             $this->configuration['project'] = $projectSlug;
-        } else {
+        } elseif (!isset($this->configuration['project'])) {
             $project = basename(realpath($configurationLoader->getRootDirectory()));
             $this->configuration['project'] = $dialog->ask($output, "<info>Project's slug</info> [<comment>$project</comment>]: ", $project);
         }
 
         // Files
-        $this->configuration['files'] = [];
         while (null !== $file = $dialog->ask($output, '<info>Pattern file</info> []: ')) {
             if (false !== $file) {
                 $this->configuration['files']['pattern'] = ['pattern' => $file];
@@ -119,6 +130,15 @@ class InitCommand extends AbstractCommand
         if (empty($this->configuration['files'])) {
             $this->configuration['files'][] = ['pattern' => 'path/to/translations.<locale>.yml'];
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get('dialog');
+        $configurationLoader = $this->get('configuration.loader');
 
         // Dump configuration
         $content = Yaml::dump($this->configuration, 3);
@@ -135,6 +155,7 @@ class InitCommand extends AbstractCommand
         $projectApi = $this->get('api')->getEntryPoint('project');
 
         try {
+            $projectSlug = $this->configuration['project'];
             $project = $projectApi->get($projectSlug);
 
             return;
