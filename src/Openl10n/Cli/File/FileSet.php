8a17b9d71e08c675f0fddfdd23310b715d44dae5
Olivier Laurendeau <o.laurendeau@gmail.com>
2015-10-30 18:08:51 +0100

    Fix var name, add typing

--- src/Openl10n/Cli/File/FileSet.php
+++ src/Openl10n/Cli/File/FileSet.php
@@ -5,7 +5,7 @@ namespace Openl10n\Cli\File;
 class FileSet
 {
     protected $rootDirectory;
-    protected $pattern;
+    protected $matcher;
     protected $options;
 
     /**
@@ -13,7 +13,7 @@ class FileSet
      * @param Matcher $matcher
      * @param array   $options
      */
-    public function __construct($rootDirectory, $matcher, array $options = array())
+    public function __construct($rootDirectory, Matcher $matcher, array $options = array())
     {
         $this->rootDirectory = $rootDirectory;
         $this->matcher = $matcher;
