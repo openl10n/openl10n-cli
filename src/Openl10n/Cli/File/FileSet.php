7256598aad4d1f3c5a79fb178e68dd8b5c358ef4
Olivier Laurendeau <o.laurendeau@gmail.com>
2015-10-31 10:35:42 +0100

    Add phpdoc to FileSet class

--- src/Openl10n/Cli/File/FileSet.php
+++ src/Openl10n/Cli/File/FileSet.php
@@ -2,10 +2,24 @@
 
 namespace Openl10n\Cli\File;
 
+/**
+ * Wrap a user defined pattern and give access to files
+ */
 class FileSet
 {
+    /**
+     * @var string
+     */
     protected $rootDirectory;
+
+    /**
+     * @var Matcher
+     */
     protected $matcher;
+
+    /**
+     * @var array
+     */
     protected $options;
 
     /**
@@ -20,11 +34,23 @@ class FileSet
         $this->options = $options;
     }
 
+    /**
+     * Retrieve files match by user pattern
+     *
+     * @return FileInfo[]
+     */
     public function getFiles()
     {
         return $this->matcher->match($this->rootDirectory);
     }
 
+    /**
+     * Retrieve options associated to this pattern
+     *
+     * @param null $key
+     *
+     * @return array
+     */
     public function getOptions($key = null)
     {
         if (null === $key) {
